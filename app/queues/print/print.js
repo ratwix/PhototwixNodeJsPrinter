84b869e502646df193a6a62cb5dd741bc51e5b11
Rathouis Charles <charles.rathouis@gmail.com>
2017-01-13 20:07:06 +0100

    Auto select print size

--- app/queues/print/print.js
+++ app/queues/print/print.js
@@ -1,5 +1,6 @@
 const logger = require("../../config/logger-config");
 const util = require("../../util/util");
+var sizeOf = require('image-size');
 
 (function (printQueue) {
   var printInterval = 500;
@@ -19,7 +20,6 @@ const util = require("../../util/util");
 
   printQueue.runPrint = function () {
     if (printQueue.toPrintQueue.length > 0) {
-
       var message = printQueue.toPrintQueue.shift();
       logger.debug("[PRINT] Shift print message " + JSON.stringify(message, 2, null));
       print(util.resultPhotoPath + '/' + message.resultFile);
@@ -27,7 +27,33 @@ const util = require("../../util/util");
   }
 
   function print(filePath) {
-    logger.info("[PRINT] file " + filePath);
+    sizeOf(filePath, function (err, dimensions) {
+      if (err) {
+        logger.error("[PRINT] error getting size " + filePath);
+      } else {
+        if (dimensions.width > dimensions.height) {
+            printLandscape(filePath);
+        } else {
+          if ((dimensions.width * 2) > dimensions.height) {
+            printPortrait(filePath);
+          } else {
+            printCutter(filePath);
+          }
+        }
+      }
+    });
+  }
+
+  function printLandscape(filePath) {
+    logger.info("[PRINT] print landscape:" + filePath);
+  }
+
+  function printPortrait(filePath) {
+    logger.info("[PRINT] print portrait:" + filePath);
+  }
+
+  function printCutter(filePath) {
+    logger.info("[PRINT] print cutter:" + filePath);
   }
 
 })(module.exports);
